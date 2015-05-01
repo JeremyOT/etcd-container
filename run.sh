5d5e123e1bc2dc76f74fd08c3ef699e0b337f2d0
Jeremy Olmsted-Thompson <jeremyot@gmail.com>
2015-05-01 11:57:29 -0400

    dont set peer urls

--- run.sh
+++ run.sh
@@ -7,8 +7,6 @@ case $1 in
     /usr/local/etcd/etcd \
       --listen-client-urls="http://0.0.0.0:2379,http://0.0.0.0:4001" \
       --advertise-client-urls="http://$ADDRESS:2379,http://$ADDRESS:4001" \
-      --listen-peer-urls="http://0.0.0.0:2380,http://0.0.0.0:7001" \
-      --initial-advertise-peer-urls="http://$ADDRESS:2380,http://$ADDRESS:7001" \
       ${ARGS}
     ;;
   auto4)
@@ -17,8 +15,6 @@ case $1 in
     /usr/local/etcd/etcd \
       --listen-client-urls="http://0.0.0.0:2379,http://0.0.0.0:4001" \
       --advertise-client-urls="http://$ADDRESS:2379,http://$ADDRESS:4001" \
-      --listen-peer-urls="http://0.0.0.0:2380,http://0.0.0.0:7001" \
-      --initial-advertise-peer-urls="http://$ADDRESS:2380,http://$ADDRESS:7001" \
       ${ARGS}
     ;;
   *)
