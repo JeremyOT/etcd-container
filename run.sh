45004cb8092b1e8b18e2d72cbd8b1af70252c218
Jeremy Olmsted-Thompson <jeremyot@gmail.com>
2016-10-21 14:46:39 -0700

    Update run.sh
    Use exec instead of running commands directly

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ case $1 in
   auto)
     ARGS=`echo ${@} | awk '{for(i=2;i<=NF;++i)print $i}'`
     ADDRESS=`/usr/bin/address`
-    /usr/local/etcd/etcd \
+    exec /usr/local/etcd/etcd \
       --listen-client-urls="http://0.0.0.0:2379,http://0.0.0.0:4001" \
       --advertise-client-urls="http://$ADDRESS:2379,http://$ADDRESS:4001" \
       ${ARGS}
@@ -12,11 +12,11 @@ case $1 in
   auto4)
     ARGS=`echo ${@} | awk '{for(i=2;i<=NF;++i)print $i}'`
     ADDRESS=`/usr/bin/address -4`
-    /usr/local/etcd/etcd \
+    exec /usr/local/etcd/etcd \
       --listen-client-urls="http://0.0.0.0:2379,http://0.0.0.0:4001" \
       --advertise-client-urls="http://$ADDRESS:2379,http://$ADDRESS:4001" \
       ${ARGS}
     ;;
   *)
-    /usr/local/etcd/etcd ${@}
+    exec /usr/local/etcd/etcd ${@}
 esac
